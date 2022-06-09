297ae6f38ac10140c22b7c21d1029be35596af38
Kai <doukai.java@gmail.com>
2022-06-09 23:09:21 +0800

    update

--- src/main/java/io/graphoenix/graphence/CustomUserStorageProvider.java
+++ src/main/java/io/graphoenix/graphence/CustomUserStorageProvider.java
@@ -2,20 +2,25 @@ package io.graphoenix.graphence;
 
 import org.keycloak.credential.CredentialInput;
 import org.keycloak.credential.CredentialInputValidator;
-import org.keycloak.models.GroupModel;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.UserModel;
+import org.keycloak.models.*;
 import org.keycloak.storage.UserStorageProvider;
+import org.keycloak.storage.group.GroupLookupProvider;
+import org.keycloak.storage.role.RoleLookupProvider;
 import org.keycloak.storage.user.UserLookupProvider;
 import org.keycloak.storage.user.UserQueryProvider;
+import org.keycloak.storage.user.UserRegistrationProvider;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class CustomUserStorageProvider implements UserStorageProvider,
         UserLookupProvider,
-        CredentialInputValidator,
-        UserQueryProvider {
+        RoleLookupProvider,
+        GroupLookupProvider,
+        UserQueryProvider,
+        UserRegistrationProvider,
+        CredentialInputValidator {
 
     @Override
     public boolean supportsCredentialType(String credentialType) {
@@ -35,6 +40,41 @@ public class CustomUserStorageProvider implements UserStorageProvider,
     @Override
     public void close() {
 
+    }
+
+    @Override
+    public GroupModel getGroupById(RealmModel realm, String id) {
+        return null;
+    }
+
+    @Override
+    public Stream<GroupModel> searchForGroupByNameStream(RealmModel realm, String search, Integer firstResult, Integer maxResults) {
+        return null;
+    }
+
+    @Override
+    public RoleModel getRealmRole(RealmModel realm, String name) {
+        return null;
+    }
+
+    @Override
+    public RoleModel getRoleById(RealmModel realm, String id) {
+        return null;
+    }
+
+    @Override
+    public Stream<RoleModel> searchForRolesStream(RealmModel realm, String search, Integer first, Integer max) {
+        return null;
+    }
+
+    @Override
+    public RoleModel getClientRole(ClientModel client, String name) {
+        return null;
+    }
+
+    @Override
+    public Stream<RoleModel> searchForClientRolesStream(ClientModel client, String search, Integer first, Integer max) {
+        return null;
     }
 
     @Override
@@ -95,5 +135,15 @@ public class CustomUserStorageProvider implements UserStorageProvider,
     @Override
     public List<UserModel> searchForUserByUserAttribute(String attrName, String attrValue, RealmModel realm) {
         return null;
+    }
+
+    @Override
+    public UserModel addUser(RealmModel realm, String username) {
+        return null;
+    }
+
+    @Override
+    public boolean removeUser(RealmModel realm, UserModel user) {
+        return false;
     }
 }
--- src/main/java/io/graphoenix/graphence/CustomUserStorageProviderFactory.java
+++ src/main/java/io/graphoenix/graphence/CustomUserStorageProviderFactory.java
@@ -1,9 +1,11 @@
 package io.graphoenix.graphence;
 
+import com.google.auto.service.AutoService;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.storage.UserStorageProviderFactory;
 
+@AutoService(UserStorageProviderFactory.class)
 public class CustomUserStorageProviderFactory implements UserStorageProviderFactory<CustomUserStorageProvider> {
 
     @Override
