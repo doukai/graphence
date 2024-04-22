614c87df73e6e53961f2fa3bc335ccb84864d32c
doukai <doukai.java@gmail.com>
2024-04-22 17:13:01 +0800

    update

--- graphence-server/src/main/java/io/graphence/server/App.java
+++ graphence-server/src/main/java/io/graphence/server/App.java
@@ -5,7 +5,7 @@ import io.graphoenix.spi.annotation.Application;
 import static io.graphoenix.core.bootstrap.App.APP;
 
 @Application
-public class GraphenceApplication {
+public class App {
 
     public static void main(String[] args) {
         APP.run(args);
