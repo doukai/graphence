d916b28516b3078a159f5e38d79321ee516789f6
doukai <doukai.java@gmail.com>
2024-08-20 17:57:08 +0800

    update

--- graphence-core/src/main/java/io/graphence/core/repository/UserRepository.java
+++ graphence-core/src/main/java/io/graphence/core/repository/UserRepository.java
@@ -8,24 +8,29 @@ import io.graphoenix.spi.annotation.GraphQLOperation;
 import io.graphoenix.spi.annotation.SelectionSet;
 import reactor.core.publisher.Mono;
 
+import java.util.Collection;
+import java.util.List;
+
 @GraphQLOperation
 public interface UserRepository {
 
     @Query(user = @UserQueryArguments(login = @StringExpression($val = "login")))
     @SelectionSet("{ id }")
+    Mono<User> getUserIdByLogin(String login);
+
+    @Query(user = @UserQueryArguments(login = @StringExpression($val = "login")))
     Mono<User> getUserByLogin(String login);
 
     @Query(user = @UserQueryArguments(id = @StringExpression($val = "id")))
-    @SelectionSet("{ id name lastName email phones }")
     Mono<User> getUserById(String id);
-
-    @Mutation(user = @UserMutationArguments($input = "user"))
-    @SelectionSet("{ id }")
-    Mono<User> updateUser(User user);
 
     @Mutation(user = @UserMutationArguments($input = "userInput"))
     @SelectionSet("{ id }")
     Mono<User> updateUser(UserInput userInput);
+
+    @Mutation(user = @UserMutationArguments($phones = "phones", where = @UserExpression(id = @StringExpression($val = "id"))))
+    @SelectionSet("{ id }")
+    Mono<User> updateUserPhones(String id, Collection<String> phones);
 
     @Mutation(user = @UserMutationArguments($salt = "salt", $hash = "hash", where = @UserExpression(id = @StringExpression($val = "id"))))
     @SelectionSet("{ id }")
