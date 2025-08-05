f51ab5bc762496178bbdbe0ab2d53490b8660841
doukai <doukai.java@gmail.com>
2025-08-05 17:52:49 +0800

    update

--- graphence-core/src/main/java/io/graphence/core/dto/objectType/Mutation.java
+++ graphence-core/src/main/java/io/graphence/core/dto/objectType/Mutation.java
@@ -148,15 +148,15 @@ public class Mutation {
   @Description("角色 权限 关系列表 变更接口")
   private Collection<RolePermissionRelation> rolePermissionRelationList;
 
+  private Boolean syncModelPolicy;
+
+  private Boolean syncPermissionRoleRelationPolicy;
+
   private User currentUserUpdate;
 
   private User currentUserResetPassword;
 
   private String login;
-
-  private Boolean syncModelPolicy;
-
-  private Boolean syncPermissionRoleRelationPolicy;
 
   public User getUser() {
     return this.user;
@@ -336,6 +336,22 @@ public class Mutation {
     this.rolePermissionRelationList = rolePermissionRelationList;
   }
 
+  public Boolean getSyncModelPolicy() {
+    return this.syncModelPolicy;
+  }
+
+  public void setSyncModelPolicy(Boolean syncModelPolicy) {
+    this.syncModelPolicy = syncModelPolicy;
+  }
+
+  public Boolean getSyncPermissionRoleRelationPolicy() {
+    return this.syncPermissionRoleRelationPolicy;
+  }
+
+  public void setSyncPermissionRoleRelationPolicy(Boolean syncPermissionRoleRelationPolicy) {
+    this.syncPermissionRoleRelationPolicy = syncPermissionRoleRelationPolicy;
+  }
+
   public User getCurrentUserUpdate() {
     return this.currentUserUpdate;
   }
@@ -358,21 +374,5 @@ public class Mutation {
 
   public void setLogin(String login) {
     this.login = login;
-  }
-
-  public Boolean getSyncModelPolicy() {
-    return this.syncModelPolicy;
-  }
-
-  public void setSyncModelPolicy(Boolean syncModelPolicy) {
-    this.syncModelPolicy = syncModelPolicy;
-  }
-
-  public Boolean getSyncPermissionRoleRelationPolicy() {
-    return this.syncPermissionRoleRelationPolicy;
-  }
-
-  public void setSyncPermissionRoleRelationPolicy(Boolean syncPermissionRoleRelationPolicy) {
-    this.syncPermissionRoleRelationPolicy = syncPermissionRoleRelationPolicy;
   }
 }
